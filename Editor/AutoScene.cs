9d3a939cf7c9becd9c6ffe249b10c5ae683bb8bf
Christophe SAUVEUR <chsxf.pro@gmail.com>
2021-06-30 11:58:51 +0200

    🔖 Version 1.1.0

---
+++ Editor/AutoScene.cs
@@ -0,0 +1,154 @@
+﻿using UnityEngine;
+using UnityEditor;
+using UnityEditor.SceneManagement;
+using System;
+using System.IO;
+
+namespace chsxf
+{
+    [InitializeOnLoad]
+    public static class AutoScene
+    {
+        private const string VERSION = "1.1.0";
+
+        private static string ProjectPath {
+            get {
+                string assetsPath = Application.dataPath;
+                return Directory.GetParent(assetsPath).FullName;
+            }
+        }
+
+        private static string PrefsKey {
+            get { return ProjectPath + ".AutoScene"; }
+        }
+
+        private static string PrefsKeyEnabled {
+            get { return PrefsKey + ".enabled"; }
+        }
+
+        private static bool Enabled {
+            get { return EditorPrefs.GetBool(PrefsKeyEnabled, true); }
+        }
+
+        static AutoScene() {
+            EditorBuildSettings.sceneListChanged += UpdatePlayModeStartScene;
+            UpdatePlayModeStartScene();
+        }
+
+        private static void UpdatePlayModeStartScene() {
+            SceneAsset sceneAsset = null;
+
+            if (Enabled) {
+                string value = EditorPrefs.GetString(PrefsKey, "none");
+                if (value == "auto") {
+                    foreach (EditorBuildSettingsScene scene in EditorBuildSettings.scenes) {
+                        if (scene.enabled) {
+                            sceneAsset = AssetDatabase.LoadAssetAtPath<SceneAsset>(scene.path);
+                            break;
+                        }
+                    }
+                }
+                else if (value != "none") {
+                    sceneAsset = AssetDatabase.LoadAssetAtPath<SceneAsset>(value);
+                }
+            }
+
+            EditorSceneManager.playModeStartScene = sceneAsset;
+        }
+
+        [MenuItem("Tools/AutoScene/Disable")]
+        private static void DisableAutoScene() {
+            EditorPrefs.SetBool(PrefsKeyEnabled, false);
+            UpdatePlayModeStartScene();
+        }
+
+        [MenuItem("Tools/AutoScene/Disable", true)]
+        private static bool CanDisableAutoScene() {
+            return Enabled;
+        }
+
+        [MenuItem("Tools/AutoScene/Enable")]
+        private static void EnableAutoScene() {
+            EditorPrefs.SetBool(PrefsKeyEnabled, true);
+            UpdatePlayModeStartScene();
+        }
+
+        [MenuItem("Tools/AutoScene/Enable", true)]
+        private static bool CanEnableAutoScene() {
+            return !Enabled;
+        }
+
+        [PreferenceItem("AutoScene")]
+        public static void AutoScenePreferences() {
+            string prefsValue = EditorPrefs.GetString(PrefsKey, "none");
+
+            EditorGUILayout.LabelField("Version", VERSION, EditorStyles.boldLabel);
+            EditorGUILayout.Space();
+
+            // Build scene list
+            string[] sceneGuids = AssetDatabase.FindAssets("t:Scene");
+            string[] scenePathes = new string[sceneGuids.Length];
+            for (int i = 0; i < sceneGuids.Length; i++) {
+                scenePathes[i] = AssetDatabase.GUIDToAssetPath(sceneGuids[i]);
+            }
+            Array.Sort(scenePathes, string.Compare);
+
+            // Finding selected index
+            int selectedIndex = 0;
+            if (prefsValue == "auto") {
+                selectedIndex = 1;
+            }
+            else {
+                int arrayIndex = Array.IndexOf(scenePathes, prefsValue);
+                if (arrayIndex >= 0) {
+                    selectedIndex = arrayIndex + 2;
+                }
+            }
+
+            string[] menuEntries = new string[scenePathes.Length + 2];
+            menuEntries[0] = "None";
+            menuEntries[1] = "Auto";
+            Array.Copy(scenePathes, 0, menuEntries, 2, scenePathes.Length);
+
+            EditorGUI.BeginChangeCheck();
+            
+            bool enabled = Enabled;
+            enabled = EditorGUILayout.Toggle("Enable AutoScene", enabled);
+            EditorGUILayout.Space();
+
+            selectedIndex = EditorGUILayout.Popup("Scene to load on Play", selectedIndex, menuEntries);
+
+            if (EditorGUI.EndChangeCheck()) {
+                if (selectedIndex == 0) {
+                    prefsValue = "none";
+                }
+                else if (selectedIndex == 1) {
+                    prefsValue = "auto";
+                }
+                else {
+                    prefsValue = menuEntries[selectedIndex];
+                }
+
+                EditorPrefs.SetString(PrefsKey, prefsValue);
+                EditorPrefs.SetBool(PrefsKeyEnabled, enabled);
+                UpdatePlayModeStartScene();
+            }
+
+            string helpBoxMessage;
+            if (selectedIndex == 0) {
+                helpBoxMessage = "The scenes currently loaded in the editor will be maintained when entering Play mode.\n\nThis is the default Unity behaviour.";
+            }
+            else if (selectedIndex == 1) {
+                helpBoxMessage = "The first enabled scene in the Build Settings box will be loaded when entering Play mode. If no such scene exists, falls back to 'None'.";
+            }
+            else {
+                helpBoxMessage = string.Format("The scene '{0}' will be loaded when entring Play mode. If the scene does not exist anymore, falls back to 'None'.", prefsValue);
+            }
+            EditorGUILayout.Space();
+            EditorGUILayout.HelpBox(helpBoxMessage, MessageType.Info, true);
+
+            EditorGUILayout.Space();
+            EditorGUILayout.LabelField("Made with ❤️ by chsxf");
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,23 @@
-# AutoScene
-AutoScene allows you to work on your Unity scene setup in the editor while loading a specific scene when entering Play mode.
+# ❓ About AutoScene
+
+AutoScene allows you to work on your scene setup in the editor while loading a specific scene when entering Play mode.
+
+## Conventions
+
+This project uses [gitmoji](https://gitmoji.dev) for its commit messages.
+
+# 🖥 Requirements
+
+AutoScene requires Unity 2018.4 or later version.
+
+# 🛠 How to Use AutoScene
+
+Once installed, AutoScene appears as a new entry in your Unity Preferences, as displayed below.
+
+![image](https://user-images.githubusercontent.com/3322862/123942946-e8926a80-d99b-11eb-95f6-289d00577570.png)
+
+AutoSceen presents only two parameters: **Enable AutoScene** and **Scene to load on Play**. For the later, three modes are available:
+
+- By selecting **None** (the default), no specific scene will be loaded and the normal behaviour of Unity when entering Play mode is maintained.
+- By selection **Auto**, the first scene in the project's Build Settings will be used. If no such scene exists (no scene in the project's Build Settings), AutoScene will fall back to **None**.
+- Any further entry refers to a specific scene in the project. By selecting a scene, it will be loaded when entering Play mode.
